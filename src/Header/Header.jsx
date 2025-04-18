cb7041a55ad506d794c18c2fdb3cdf32114b9163
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-04-18 23:00:41 +0300

    hamburger menu functional added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,25 @@
 import "./Header.css";
+import { useState } from "react";
+
 const Header = ({ name }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header>
       <div className="logo">
         <h2>{name}</h2>
       </div>
       <nav>
-        <ul>
+        <div className="hamburger" onClick={toggleMobileMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <ul className={`nav-links ${isMobileMenuOpen ? "active mobile" : ""}`}>
           <li>Home</li>
           <li>Persons</li>
           <li>Books</li>
